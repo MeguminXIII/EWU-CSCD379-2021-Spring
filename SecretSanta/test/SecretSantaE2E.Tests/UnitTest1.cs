f81d7e91ef287d1a8d1a0ac59a97edf06731015f
Ryan Golden <rgolden5@eagles.ewu.edu>
2021-05-16 21:09:44 -0700

    Created E2E test project and added it to .sln

---
+++ SecretSanta/test/SecretSantaE2E.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SecretSantaE2E.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
